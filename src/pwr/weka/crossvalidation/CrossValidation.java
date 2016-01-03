a3bea2b5651a18280035ea97c7f0be2f57f0c432
maciejsobotka <sobot92@tlen.pl>
2016-01-03 21:22:11 +0100

    Folds random creation

--- src/pwr/weka/crossvalidation/CrossValidation.java
+++ src/pwr/weka/crossvalidation/CrossValidation.java
@@ -1,5 +1,33 @@
 package pwr.weka.crossvalidation;
 
-public class CrossValidation {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+
+public class CrossValidation {
+	private Instances[] folds;
+	
+	public void doCrossValidation(Instances data, Classifier classifier, int numFolds, int numReps){
+		generateFolds(data, numFolds);
+	}
+	
+	private void generateFolds(Instances data, int numFolds){
+		folds = new Instances[numFolds];
+		for(int i=0; i<folds.length; ++i)
+			folds[i] = new Instances(data, data.numInstances()/numFolds+1);
+		List<Integer> rand = new ArrayList<>();
+		for(int i=0; i<data.numInstances(); ++i)
+			rand.add(i);
+		Collections.shuffle(rand);
+		
+		int n=0;
+		for(int i=0; i<data.numInstances(); ++i){
+			folds[n].add(data.get(rand.get(i)));
+			if(n==9) n=0;
+			else n++;
+		}
+	}
 }
--- src/pwr/weka/main/ProgramMain.java
+++ src/pwr/weka/main/ProgramMain.java
@@ -1,5 +1,39 @@
 package pwr.weka.main;
 
-public class ProgramMain {
+import pwr.weka.crossvalidation.CrossValidation;
+import pwr.weka.utils.LoadInstances;
+import weka.classifiers.Classifier;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.Instances;
 
+/**
+ * Place where the program starts.
+ * Two available arguments
+ * 1 - number of folds to use in cross-validation
+ * 2 - number of cross-validation repeats
+ * 
+ * @author Sobot
+ */
+public class ProgramMain {
+	public static void main(String[] args){
+		int numFolds = 10;
+		int numReps = 1;
+		if(args.length == 1) {numFolds = Integer.parseInt(args[0]); numReps = 1;}
+		if(args.length == 2) {
+			numFolds = Integer.parseInt(args[0]);
+			numReps = Integer.parseInt(args[1]);
+		}
+		
+		LoadInstances loader = new LoadInstances();
+		Instances data;
+		try {
+			data = loader.Load("195420L4_1.arff");
+			CrossValidation cv = new CrossValidation();
+			Classifier classifier = (Classifier)new NaiveBayes();
+			cv.doCrossValidation(data, classifier , numFolds, numReps);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
